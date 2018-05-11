ee8a03aa99a46a18e4610b2fd2a12caba80aacc2
Mutlucan Tokat <mtokat1@gmail.com>
2018-05-11 18:18:33 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,19 +8,6 @@ import subprocess
 
 db = getvideo.Database()
 
-#sql_insert = "INSERT INTO video (title, description, source_id, video_url) VALUES (%s,%s,%s,%s)"
-#sql_select = "SELECT * FROM video"
-
-#print(db.query_insert(sql_insert, ['212d','ded','f3ef34','f3f']))
-
-
-#video = getvideo.get_video()
-#video.download_file('http://static.59saniye.com/videos/2018/04/10/20180410155616-6259_1080.mp4', '/var/www/html/GetVideo/video/vid2.mp4')
-
-#a = 'http://static.59saniye.com/videos/2018/04/10/20180410155616-6259_1080.mp4'
-#print(a.split('/')[-1])
-
-
 videos = getvideo.get_video()
 
 last_videos = videos.get_last_uploaded_videos()
@@ -46,6 +33,6 @@ for video in last_videos:
 #Closes open database connections
 db.close_db_connection()
 #Deletes all files and video download directory
-#shutil.rmtree('/var/www/html/GetVideo/video/')
+shutil.rmtree('/var/www/html/GetVideo/video/')
 #Creates an empty directory after deleting it
-#os.makedirs('/var/www/html/GetVideo/video/', exist_ok=True)
+os.makedirs('/var/www/html/GetVideo/video/', exist_ok=True)
